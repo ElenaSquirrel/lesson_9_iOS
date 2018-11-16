ea3a5cbdda9143aa89f6d290a8910edfc7597447
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-11-15 20:25:39 -0500

    Initial Commit

---
+++ lesson_9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson_9
+//
+//  Created by Elena Melnikova on 2018-11-15.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
